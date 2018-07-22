e102313712070fb81041671242975160044df75a
Jan CW Kroeze <thejcwk@gmail.com>
2018-07-23 00:04:09 +0200

    It's probably safer to white list characters instead.

--- index.js
+++ index.js
@@ -12,14 +12,23 @@ http.createServer((request, response) => {
     let url_parts = url.parse(request.url, true);
     let args = [];
     for (let p in url_parts.query) {
-        let param_key = p.replace('"', '\\"').replace("'", "\\'");
-        let param_value = url_parts.query[p].replace('"', '\\"').replace("'", "\\'");
+        let param_key = p;
+        let param_value = url_parts.query[p];
+        let whitelist = (s) => {
+            let re = /[^A-Za-z0-9\-]/g;
+            if (s.search(re) !== -1) {
+                response.writeHead(400);
+                response.end("Illegal argument: only characters A-Z, a-z, 0-9, and - are allowed.")
+            }
+        };
+        whitelist(param_key);
+        whitelist(param_value);
         args.push(`"--${param_key}"`);
         args.push(`"${param_value}"`);
     }
     args.push('> ./www/build.html');
 
-    console.log(`Received: ${request.url}`)
+    console.log(`Received: ${request.url}`);
 
     try {
         child_process.spawnSync(
